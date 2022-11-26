f629203256c1c863b8a9d99284b37ec9ddbdd581
Money Hoarders Global Network <94566378+moneyhoardersglobalnetwork@users.noreply.github.com>
2022-11-26 11:20:57 -0500

    Moved BopToken.sol to repo
    The original source contract was not in the Hackathon repo. Moved contract into repo TO?DO add flatten verified abi from Polygonscan.

---
+++ src/contracts/BopToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BopToken is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
+    constructor() ERC20("BOP by MHGN", "") ERC20Permit("BOP by MHGN") {
+        _mint(msg.sender, 1200000000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
